01efcf4ff4c2ff69eed275b5766e5e98e86ff270
dieguitoml <124363512+dieguitoml@users.noreply.github.com>
2025-06-27 13:51:29 +0200

    Create Lock.java

---
+++ Concurrente/Locks/Lock.java
@@ -0,0 +1,14 @@
+package Concurrente.Locks;
+
+public abstract class Lock {
+
+	public abstract void takeLock(int id);
+	public abstract void releaseLock(int id);
+	public void takeLock() {
+		// TODO Auto-generated method stub
+	}
+	public void releaseLock() {
+		// TODO Auto-generated method stub
+	}
+	
+}
